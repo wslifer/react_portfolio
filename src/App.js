e893ec96a8f1f1170c8ee18827e44cf983605505
Wesley Slifer <wesleyslifer@ymail.com>
2020-08-02 17:36:18 -0500

    fixing display of pages in router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import About from "./pages/About";
-import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
 import NoMatch from "./pages/NoMatch";
 
 function App() {
   return (
     <Router>
-      <Route exact path="/" component={About} />
-      <Route exact path="/portfolio" component={Portfolio} />
-      <Route exact path="/contact" component={Contact} />
-      <Route component={NoMatch} />
+      <Switch>
+        <Route exact path="/">
+          <About />
+        </Route>
+        <Route path="/Portfolio">
+          <Portfolio />
+        </Route>
+        <Route path="/Contact">
+          <Contact />
+        </Route>
+        <Route path="*">
+          <NoMatch />
+        </Route>
+      </Switch>
     </Router>
   );
 }
--- src/pages/NoMatch.js
+++ src/pages/NoMatch.js
@@ -7,8 +7,8 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
 
 const useStyles = makeStyles({
   nav: {
@@ -33,18 +33,28 @@ const useStyles = makeStyles({
     height: 450,
     width: 450,
   },
+  button: {
+    marginLeft: "auto",
+  },
 });
 
 export default function NoMatch() {
   const style = useStyles();
+
   return (
     <Grid>
       <AppBar className={style.nav} position="static">
         <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h5">Slifer Portfolio</Typography>
+          <ButtonGroup
+            className={style.button}
+            size="large"
+            aria-label="large outlined primary button group"
+          >
+            <Button>About</Button>
+            <Button>Contact</Button>
+            <Button>Portfolio</Button>
+          </ButtonGroup>
         </Toolbar>
       </AppBar>
       <Card className={style.card}>
