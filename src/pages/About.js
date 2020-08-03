ff1b3a6744756533e8586718c9f9136627ce5523
Wesley Slifer <wesleyslifer@ymail.com>
2020-08-03 12:25:02 -0500

    adding picture to about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,10 +6,30 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 
-const useStyles = makeStyles({});
+const useStyles = makeStyles({
+  picture: {
+    height: 350,
+    paddingTop: "56.25%",
+  },
+});
 
 export default function About() {
   const style = useStyles();
 
-  return <Grid></Grid>;
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={6}>
+        <Card item xs={6}>
+          <CardMedia
+            className={style.picture}
+            image={require("../assets/images/profilepicture.png")}
+            title="Profile Picture"
+          />
+        </Card>
+      </Grid>
+      <Grid item xs={6}>
+        <CardContent item xs={6}></CardContent>
+      </Grid>
+    </Grid>
+  );
 }
