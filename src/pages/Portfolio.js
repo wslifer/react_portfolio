f6219a61d1f88631419142d43eedd4bf322c41a9
wslifer <wesleyslifer@ymail.com>
2020-08-09 19:57:35 -0500

    updating portfolio page

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -4,15 +4,27 @@ import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles({
   project: {
-    color: "white",
-    fontSize: 28,
     background: "#615e57",
     borderRadius: 3,
     height: 200,
     marginBottom: 20,
+    display: "flex",
+  },
+  projectPic: {
+    height: 150,
+    width: 200,
+    borderRadius: 20,
+    marginTop: 20,
+  },
+  button: {
+    color: "white",
+    background: "#0276aa",
+    height: 75,
+    margin: "auto",
   },
 });
 
@@ -22,16 +34,86 @@ export default function Portfolio() {
   return (
     <Grid container spacing={3}>
       <Grid item xs={4}>
-        <Card className={style.project}></Card>
-        <Card className={style.project}></Card>
+        <Card className={style.project}>
+          <Button
+            className={style.button}
+            href="https://wslifer.github.io/Project_1/"
+          >
+            Bites and Quarantinis
+          </Button>
+          <CardMedia
+            image={require("../assets/images/Project1.PNG")}
+            className={style.projectPic}
+          />
+        </Card>
+        <Card className={style.project}>
+          <Button
+            className={style.button}
+            href="https://wslifer.github.io/Project_1/"
+          >
+            Bites and Quarantinis
+          </Button>
+          <CardMedia
+            image={require("../assets/images/Project1.PNG")}
+            className={style.projectPic}
+          />
+        </Card>
       </Grid>
       <Grid item xs={4}>
-        <Card className={style.project}></Card>
-        <Card className={style.project}></Card>
+        <Card className={style.project}>
+          {" "}
+          <Button
+            className={style.button}
+            href="https://wslifer.github.io/Project_1/"
+          >
+            Bites and Quarantinis
+          </Button>
+          <CardMedia
+            image={require("../assets/images/Project1.PNG")}
+            className={style.projectPic}
+          />
+        </Card>
+        <Card className={style.project}>
+          {" "}
+          <Button
+            className={style.button}
+            href="https://wslifer.github.io/Project_1/"
+          >
+            Bites and Quarantinis
+          </Button>
+          <CardMedia
+            image={require("../assets/images/Project1.PNG")}
+            className={style.projectPic}
+          />
+        </Card>
       </Grid>
       <Grid item xs={4}>
-        <Card className={style.project}></Card>
-        <Card className={style.project}></Card>
+        <Card className={style.project}>
+          {" "}
+          <Button
+            className={style.button}
+            href="https://wslifer.github.io/Project_1/"
+          >
+            Bites and Quarantinis
+          </Button>
+          <CardMedia
+            image={require("../assets/images/Project1.PNG")}
+            className={style.projectPic}
+          />
+        </Card>
+        <Card className={style.project}>
+          {" "}
+          <Button
+            className={style.button}
+            href="https://wslifer.github.io/Project_1/"
+          >
+            Bites and Quarantinis
+          </Button>
+          <CardMedia
+            image={require("../assets/images/Project1.PNG")}
+            className={style.projectPic}
+          />
+        </Card>
       </Grid>
     </Grid>
   );
