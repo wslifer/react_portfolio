9a82111e3bd66d369e40a45ee0a1f523485f2158
wslifer <wesleyslifer@ymail.com>
2020-08-09 19:44:20 -0500

    removing unused imports

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,8 +3,6 @@ import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles({
