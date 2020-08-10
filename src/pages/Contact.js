ca24dc8b3b7662d298144dcbae6977a0fdd547aa
wslifer <wesleyslifer@ymail.com>
2020-08-09 19:35:20 -0500

    contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,11 +5,60 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import { Link } from "react-router-dom";
+import { AutoComplete } from "material-ui";
 
-const useStyles = makeStyles({});
+const useStyles = makeStyles({
+  contact: {
+    color: "white",
+    fontSize: 28,
+    background: "#615e57",
+    borderRadius: 3,
+    height: 450,
+    width: 750,
+    marginLeft: "auto",
+    marginRight: "auto",
+  },
+  button: {
+    background: "#0276aa",
+    color: "white",
+    width: 300,
+    height: 75,
+    display: "flex",
+    marginTop: 20,
+    marginRight: "auto",
+    marginLeft: "auto",
+  },
+});
 
 export default function Contact() {
   const style = useStyles();
 
-  return <Grid></Grid>;
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs>
+        <Card item xs className={style.contact}>
+          <CardContent>
+            <Button className={style.button} href="https://github.com/wslifer">
+              Github
+            </Button>
+            <Button
+              className={style.button}
+              href="https://www.linkedin.com/in/wesley-slifer"
+            >
+              LinkedIn
+            </Button>
+            <Button
+              className={style.button}
+              href="https://docs.google.com/document/d/1xJyHqfyaGYW9o0bk_da3j560ggTP2LP6BtKD9JMovJM/edit?usp=sharing"
+            >
+              Resume
+            </Button>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
+  );
 }
--- src/pages/NoMatch.js
+++ src/pages/NoMatch.js
@@ -4,7 +4,6 @@ import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
-import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles({
   card: {
