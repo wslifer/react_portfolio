dc6409feebbc474f9368e1d3a16b450c70587dd1
Wesley Slifer <wesleyslifer@ymail.com>
2020-09-04 14:57:35 -0500

    updating portfolio page

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -7,24 +7,11 @@ import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles({
-  project: {
-    background: "#615e57",
-    borderRadius: 3,
-    height: 200,
-    marginBottom: 20,
-    display: "flex",
-  },
-  projectPic: {
-    height: 150,
-    width: 200,
-    borderRadius: 20,
-    marginTop: 20,
-  },
-  button: {
-    color: "white",
-    background: "#0276aa",
-    height: 75,
-    margin: "auto",
+  paper: {
+    textAlign: "center",
+    "& .MuiPaper-rounded": {
+      borderRadius: 0,
+    },
   },
 });
 
@@ -32,89 +19,126 @@ export default function Portfolio() {
   const style = useStyles();
 
   return (
-    <Grid container spacing={3}>
-      <Grid item xs={4}>
-        <Card className={style.project}>
-          <Button
-            className={style.button}
-            href="https://wslifer.github.io/Project_1/"
-          >
-            Bites and Quarantinis
-          </Button>
-          <CardMedia
-            image={require("../assets/images/Project1.PNG")}
-            className={style.projectPic}
-          />
-        </Card>
-        <Card className={style.project}>
-          <Button
-            className={style.button}
-            href="https://wslifer.github.io/Project_1/"
-          >
-            Bites and Quarantinis
-          </Button>
-          <CardMedia
-            image={require("../assets/images/Project1.PNG")}
-            className={style.projectPic}
-          />
-        </Card>
-      </Grid>
-      <Grid item xs={4}>
-        <Card className={style.project}>
-          {" "}
-          <Button
-            className={style.button}
-            href="https://wslifer.github.io/Project_1/"
-          >
-            Bites and Quarantinis
-          </Button>
-          <CardMedia
-            image={require("../assets/images/Project1.PNG")}
-            className={style.projectPic}
-          />
-        </Card>
-        <Card className={style.project}>
-          {" "}
-          <Button
-            className={style.button}
-            href="https://wslifer.github.io/Project_1/"
-          >
-            Bites and Quarantinis
-          </Button>
-          <CardMedia
-            image={require("../assets/images/Project1.PNG")}
-            className={style.projectPic}
-          />
-        </Card>
-      </Grid>
-      <Grid item xs={4}>
-        <Card className={style.project}>
-          {" "}
-          <Button
-            className={style.button}
-            href="https://wslifer.github.io/Project_1/"
-          >
-            Bites and Quarantinis
-          </Button>
-          <CardMedia
-            image={require("../assets/images/Project1.PNG")}
-            className={style.projectPic}
-          />
-        </Card>
-        <Card className={style.project}>
-          {" "}
-          <Button
-            className={style.button}
-            href="https://wslifer.github.io/Project_1/"
-          >
-            Bites and Quarantinis
-          </Button>
-          <CardMedia
-            image={require("../assets/images/Project1.PNG")}
-            className={style.projectPic}
-          />
-        </Card>
-      </Grid>
-    </Grid>
+    <Section style={{ background: "#f5f5f5" }}>
+      <Title>
+        <Typography>Projects</Typography>
+      </Title>
+
+      <div className={classes.grid}>
+        <Grid container spacing={3}>
+          <Grid item md={4} sm={6} xs={12}>
+            <Paper className={classes.paper}>
+              <Card
+                image={}
+                title=""
+                alt=""
+                tools=""
+                github=""
+                heroku=""
+                locked={false}
+              />
+            </Paper>
+          </Grid>
+
+          <Grid item md={4} sm={6} xs={12}>
+            <Paper className={classes.paper}>
+              <Card
+                image={}
+                title=""
+                alt=""
+                text=""
+                github=""
+                heroku=""
+                locked={false}
+              />
+            </Paper>
+          </Grid>
+
+          <Grid item md={4} sm={6} xs={12}>
+            <Paper className={classes.paper}>
+              <Card
+                image={}
+                title=""
+                alt=""
+                text=""
+                github=""
+                heroku=""
+                locked={false}
+              />
+            </Paper>
+          </Grid>
+
+          <Grid item md={4} sm={6} xs={12}>
+            <Paper className={classes.paper}>
+              <Card
+                image={}
+                title=""
+                alt=""
+                text=""
+                github=""
+                heroku=""
+                locked={false}
+              />
+            </Paper>
+          </Grid>
+
+          <Grid item md={4} sm={6} xs={12}>
+            <Paper className={classes.paper}>
+              <Card
+                image={}
+                title=""
+                alt=""
+                text=""
+                github=""
+                heroku=""
+                locked={false}
+              />
+            </Paper>
+          </Grid>
+
+          <Grid item md={4} sm={6} xs={12}>
+            <Paper className={classes.paper}>
+              <Card
+                image={}
+                title=""
+                alt=""
+                text=""
+                github=""
+                heroku=""
+                locked={false}
+              />
+            </Paper>
+          </Grid>
+
+          <Grid item md={4} sm={6} xs={12}>
+            <Paper className={classes.paper}>
+              <Card
+                image={}
+                title=""
+                alt=""
+                text=""
+                github=""
+                heroku=""
+                locked={false}
+              />
+            </Paper>
+          </Grid>
+
+          <Grid item md={4} sm={6} xs={12}>
+            <Paper className={classes.paper}>
+              <Card
+                image={}
+                title=""
+                alt=""
+                text=""
+                github=""
+                heroku=""
+                locked={false}
+              />
+            </Paper>
+          </Grid>
+        </Grid>
+      </div>
+    </Section>
   );
 }
