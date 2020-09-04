5c7216759d48121a66802b9ef6cb227ff0c3c688
Wesley Slifer <wesleyslifer@ymail.com>
2020-09-04 15:00:51 -0500

    fixing imports

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,10 +1,6 @@
 import React from "react";
-import Grid from "@material-ui/core/Grid";
+import { Grid, Card, Paper, Button } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles({
   paper: {
