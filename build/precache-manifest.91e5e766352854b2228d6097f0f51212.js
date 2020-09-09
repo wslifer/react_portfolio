61c5ecedf63bc55766b9cc3c045d9a70ceb240dc
Wesley Slifer <wesleyslifer@ymail.com>
2020-09-09 15:22:52 -0500

    redeploy

---
+++ build/precache-manifest.91e5e766352854b2228d6097f0f51212.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9f8fb8a0592ee0e51c5aa0c9bdfe6f3",
+    "url": "/react_portfolio/index.html"
+  },
+  {
+    "revision": "24038c7a955ab5908d3f",
+    "url": "/react_portfolio/static/js/2.a1f6b961.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/react_portfolio/static/js/2.a1f6b961.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26ddd08be9ce8e6eded2",
+    "url": "/react_portfolio/static/js/main.b5d31f32.chunk.js"
+  },
+  {
+    "revision": "9516736bfd4120688efb",
+    "url": "/react_portfolio/static/js/runtime-main.aff8cc16.js"
+  },
+  {
+    "revision": "dfe3b56a3c17b8fb639b076c9deae19a",
+    "url": "/react_portfolio/static/media/404.dfe3b56a.png"
+  },
+  {
+    "revision": "138a96660b593a8156744e6cce67a00a",
+    "url": "/react_portfolio/static/media/CodingQuiz.138a9666.png"
+  },
+  {
+    "revision": "646e62e267e4033502bda6f557d21902",
+    "url": "/react_portfolio/static/media/EmployeeTracker.646e62e2.png"
+  },
+  {
+    "revision": "43987053da1745cf264b98460a57e547",
+    "url": "/react_portfolio/static/media/ItTakesAVillage.43987053.png"
+  },
+  {
+    "revision": "47764d9a12375420fab22e6f6505e70b",
+    "url": "/react_portfolio/static/media/ProjectYou.47764d9a.png"
+  },
+  {
+    "revision": "21041ff46c7765633950ac22c5cabc5b",
+    "url": "/react_portfolio/static/media/Quarantinis.21041ff4.PNG"
+  },
+  {
+    "revision": "7449fafcd26a79b40b9eafb67d72c314",
+    "url": "/react_portfolio/static/media/TeamProfileGenerator.7449fafc.png"
+  },
+  {
+    "revision": "edeb14284504058b740f06d5740ac97e",
+    "url": "/react_portfolio/static/media/profilepicture.edeb1428.png"
+  }
+]);
